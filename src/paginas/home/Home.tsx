8eb8a83ae47d35f7c91618f78e2d43f011486a4c
kaiquesilvadev <j.kaique1570@gmail.com>
2023-06-13 13:58:37 -0300

    init project

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './home.css'
+
+function Home() {
+    return(
+        <>
+            <h1 className="tituto">Home</h1>
+            <img src="https://i.imgur.com/H88yIo2.png" alt="imagem tela inicial"/>
+        </>
+    );
+}
+
+export default Home;
