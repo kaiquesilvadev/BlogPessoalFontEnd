9c6ad5054660eba07e58102623b4f660cacd4d63
kaiquesilvadev <j.kaique1570@gmail.com>
2023-06-20 16:01:56 -0300

    criação da pagina cadastrare e consumo de api no login e no cadastrar,

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Footer from './components/estaticos/footer/Footer';
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Login from './paginas/login/Logon';
 import { ClassNames } from '@emotion/react';
+import CadastroUsuario from './paginas/cadastroUsuario/CadastroUsuario'
 
 function App() {
   return (
@@ -16,7 +17,7 @@ function App() {
             <Route path="/" element={<Login />} />
             <Route path="/login" element={<Login />} />
             <Route path="/home" element={<Home />} />
-            {/* <Route path="/cadastro" element={<CadastroUsuario />} /> */}
+            <Route path="/cadastro" element={<CadastroUsuario />} />
           </Routes>
         </div>
         <Footer />
