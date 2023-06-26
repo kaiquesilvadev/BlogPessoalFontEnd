c3f958a09236542263e7ce53e9233b5529f8f821
kaiquesilvadev <j.kaique1570@gmail.com>
2023-06-26 15:10:32 -0300

    criação do cadastro tema e do cadastro de postagem

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,11 @@ import Login from './paginas/login/Login';
 import CadastroUsuario from './paginas/cadastroUsuario/CadastroUsuario'
 import ListaTema from './components/temas/listaTema/ListaTema';
 import ListaPostagem from './components/postagens/listaPostagem/ListaPostagem';
+import CadastroPost from './components/postagens/cadastroPost/CadastroPost';
+import CadastroTema from './components/temas/cadastroTema/CadastroTema';
+import DeletarPostagem from './components/postagens/deletarPostagem/DeletarPostagem';
+import DeletarTema from './components/temas/deletarTema/DeletarTema';
+
 
 function App() {
   return (
@@ -15,19 +20,34 @@ function App() {
         <NavBar />
         <div style={{ minHeight: '100vh' }}>
           <Routes> // Antigo Switch
-          <Route path="/" element={<Login  />} />
 
-            <Route path="/home" element={<Home />} />
+            <Route path="/" element={<Login />} />
 
             <Route path="/login" element={<Login />} />
 
-            <Route path="/cadastrousuario" element={<CadastroUsuario/>} />
+            <Route path="/home" element={<Home />} />
+
+            <Route path="/cadastrousuario" element={<CadastroUsuario />} />
 
             <Route path="/temas" element={<ListaTema />} />
 
             <Route path="/posts" element={<ListaPostagem />} />
-                      </Routes>
-                    </div>
+
+            <Route path="/formularioPostagem" element={<CadastroPost />} />
+
+            <Route path="/formularioPostagem/:id" element={<CadastroPost />} />
+
+            <Route path="/formularioTema" element={<CadastroTema />} />
+
+            <Route path="/formularioTema/:id" element={<CadastroTema />} />
+
+            <Route path="/deletarPostagem/:id" element={<DeletarPostagem />} />
+
+            <Route path="/deletarTema/:id" element={<DeletarTema />} />
+
+
+          </Routes>
+        </div>
         <Footer />
       </BrowserRouter>
     </>
