2645dbeb671f40606afe4c3b5ed3137d1a321429
kaiquesilvadev <j.kaique1570@gmail.com>
2023-07-04 14:21:58 -0300

    deploy , projeto final update1

--- src/components/postagens/tebPostagem/TabPostagem.tsx
+++ src/components/postagens/tebPostagem/TabPostagem.tsx
@@ -7,7 +7,7 @@ import './TabPostagem.css';
 
 function TabPostagem() {
   const [value, setValue] = useState('1')
-  function handleChange(event: React.ChangeEvent<{}>, newValue: string){
+  function handleChange(e: React.ChangeEvent<{}>, newValue: string){
     setValue(newValue);
   }
 return (
